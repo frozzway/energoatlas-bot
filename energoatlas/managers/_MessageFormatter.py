4540d5bd203e328cf0b310d2511db448845aa370
Александр Окунев <a.okunev@mininform74.ru>
2024-05-28 09:49:22 +0500

    Поправил escaping MarkdownV2

--- energoatlas/managers/_MessageFormatter.py
+++ energoatlas/managers/_MessageFormatter.py
@@ -6,13 +6,20 @@ class MessageFormatter:
     def notification_message(device_logs: list[DeviceWithLogs]) -> TelegramMessageParams:
         items = []
         for device in device_logs:
-            header = (f'На устройстве: *{device.device.name}*, установленном на объекте *{device.device.object_name}* '
-                      f'по адресу *{device.device.object_address}* обнаружены следующие срабатывания:\n\n')
-            messages = [f'{log.latch_message}\n{log.latch_dt.strftime("%Y-%m-%d %H:%M:%S")}' for log in device.logs]
+            device_name = MessageFormatter.escape_markdown(device.device.name)
+            object_name = MessageFormatter.escape_markdown(device.device.object_name)
+            object_address = MessageFormatter.escape_markdown(device.device.object_address)
+            header = (f'На устройстве: *{device_name}*, установленном на объекте *{object_name}* '
+                      f'по адресу *{object_address}* обнаружены следующие срабатывания:\n\n')
+            messages = [(f'{MessageFormatter.escape_markdown(log.latch_message)}\n'
+                         f'{MessageFormatter.escape_markdown(log.latch_dt.strftime("%Y-%m-%d %H:%M:%S"))}') for log in device.logs]
             body = '\n\n'.join(messages)
             items.append(header + body)
-        return TelegramMessageParams(text=MessageFormatter.escape_markdown("\n\n".join(items)), parse_mode='MarkdownV2')
+        return TelegramMessageParams(text="\n\n".join(items), parse_mode='MarkdownV2')
 
     @staticmethod
     def escape_markdown(text: str) -> str:
-        return text.replace('-', '\\-').replace('(', '\\(').replace(')', '\\)')
+        escaped_chars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']
+        for char in escaped_chars:
+            text = text.replace(char, '\\' + char)
+        return text
